472e638b9f6cb2c11ccdd27b9ab0ed4fbfa13ccb
Abhishek Gupta <awittlegupta@gmail.com>
2017-08-08 15:12:32 -0700

    queries 14-35

--- oracle_db.sql
+++ oracle_db.sql
@@ -175,3 +175,101 @@ FROM emp;
 SELECT deptno, sal
 FROM emp
 ORDER BY deptno, sal ASC;
+
+
+-- 24. Print "<ename> IS THE NAME and their job is: <job>" 
+-- 	   using SRF functions wherever applicable. Wrap everything in
+--     CONCATS. Find a way to only capitalize the 1st letter in name.
+
+SELECT CONCAT(CONCAT(CONCAT(INITCAP(LOWER(ename)), UPPER(' is the name ')), LOWER(' and their job is: ')), job)
+FROM emp;
+
+
+-- 25. Find the names of everyone whose name is less than 5 characters.
+-- 	   Their name should only have the 1st letter capitalized and all names
+-- 	   should be in alphabetical order from A-Z.
+
+SELECT INITCAP(LOWER(ename)) AS "Name", LENGTH(ename) AS "Name Length"
+FROM emp
+WHERE LENGTH(ename) < 5
+ORDER BY "Name";
+
+
+-- 26. Assuming everyone is a male in the emp table, add a 'Mr. ' prefix to
+--	   everyone's name using LPAD.
+
+SELECT LPAD(ename, LENGTH(ename)+4, 'Mr. ')
+FROM emp;
+
+
+-- 27. Find everyone who was hired in 1982.
+
+SELECT *
+FROM emp
+WHERE trunc(hiredate, 'YEAR') = '01/01/1982';
+
+
+-- 28. Print out today's date in MM-DD-YYYY format.
+
+SELECT TO_CHAR(CURRENT_DATE, 'MM-DD-YYYY') AS "TODAY"
+FROM dual;
+
+
+-- 29. Print today's date in 'ddth of month, yyy' format.
+
+SELECT TO_CHAR(CURRENT_DATE, 'DDth "of" MONTH, YYYY')
+FROM dual;
+
+
+-- 30. Represent 23596.34 as $23,596.34
+
+SELECT TO_CHAR(23596.34, '$99,999.99')
+FROM dual;
+
+
+-- 31. Print out the salaries of all employees formatted nicely with commas and 
+--	   dollar signs.
+
+SELECT ename AS "Employee Name", TO_CHAR(sal, '$99,999.00') AS "Salary"
+FROM emp
+ORDER BY "Salary" DESC;
+
+
+-- 32. Print all details of all employees. If their MGR id is NULL, make it say
+--	   '0' and if their commission is NULL, make it say '0.' Use ONLY CASE
+--     statements.
+
+SELECT empno, ename, job,
+	CASE
+		WHEN mgr IS NULL THEN 0
+		ELSE mgr
+	END AS "Manager ID",
+	CASE
+		WHEN comm IS NULL THEN 0
+		ELSE comm
+	END AS "Commission"
+FROM emp;	
+
+
+-- 33. Do the same thing you did in 32. except use NVL and say NONE or NO DATA for
+--     mgr and comm if NULL.
+
+SELECT empno, ename, job, NVL(TO_CHAR(mgr), 'NONE') AS "Manager", NVL(TO_CHAR(comm), 'NO DATA') AS "Commission"
+FROM emp;
+
+
+-- 34. Print 3 columns: ename, length of ename, and then if the length is ename is 
+--	   5, print NULL. Otherwise, just print the length again.
+
+SELECT ename, length(ename), NULLIF(length(ename), 5)
+FROM emp;
+
+
+-- 35. Following up with problem 34, wherever there is a NULL value, make it instead 
+--	   say 'LENGTH IS 5.'
+
+SELECT ename, length(ename) AS "Length", NVL(TO_CHAR(NULLIF(length(ename), 5)), 'LENGTH IS 5') AS "Length in Chars"
+FROM emp;
+
+
+-- 36. 
